ce665bb9a91d4100f1f56ea334164c4bed94a28f
Amul <amul67142@gmail.com>
2023-08-03 21:58:04 +0530

    totdo backend cruid completed

--- controllers/task.js
+++ controllers/task.js
@@ -1,4 +1,4 @@
-import ErrorHandler from "../middlewares/error.js";
+
 import { Task } from "../models/task.js";
 
 export const newTask=async(req,res,next)=>{
@@ -23,22 +23,32 @@ export const newTask=async(req,res,next)=>{
 
 
 export const getMyTask=async(req,res)=>{
-        const userId= req.user._id;
+   try {
+    
+    const userId= req.user._id;
 
-        const tasks= await Task.find({user : userId});
+    const tasks= await Task.find({user : userId});
 
-        res.status(200).json({
-            success:true,
-            tasks
-        })
+    res.status(200).json({
+        success:true,
+        tasks
+    })
+   } catch (error) {
+
+    next(error)
+    
+   }
 }
 
 export const updateTask=async(req,res)=>{
-
+        try{
    const task= await Task.findById(req.params.id);
 
    if(!task)
-   return next(new Error("invalid id"))
+   return res.status(404).json({
+    success: false,
+    message: "Task not found",
+});
 
    task.isCompleted=!task.isCompleted;
    await task.save();
@@ -48,26 +58,46 @@ export const updateTask=async(req,res)=>{
     success:true,
     message:"updated"
    })
+}catch(error){
+    res.status(500).json({
+        success: false,
+        message: "invalid id",
+        error: error.message,
+    });
+
+}
     
 
  
 }
 
-export const deleteTask=async(req,res)=>{
-    const task= await Task.findById(req.params.id);
+export const deleteTask = async (req, res) => {
+    try {
+        // Find the task in the database by its ID
+        const task = await Task.findById(req.params.id);
 
-    if(!task)
-   return res.status(err.statusCode).json({
-    success:false,
-    message:err.message,
-});
+        // If the task is not found in the database, return an error response
+        if (!task) {
+            return res.status(404).json({
+                success: false,
+                message: "Task not found",
+            });
+        }
 
-    await task.deleteOne();
-   
-    res.status(200).json({
-        success:true,
-        message:"deleted"
-       })
+        // If the task is found, delete it from the database using deleteOne()
+        await task.deleteOne();
 
-
+        // Return a success response indicating the task was deleted successfully
+        res.status(200).json({
+            success: true,
+            message: "Task deleted",
+        });
+    } catch (error) {
+        // Handle any MongoDB-related errors
+        res.status(500).json({
+            success: false,
+            message: "An error occurred while deleting the task",
+            error: error.message,
+        });
+    }
 }
